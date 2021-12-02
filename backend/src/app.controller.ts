6b49d85222c756cd164774c15afacc0835975446
Conrad Scherb <68687721+conrad-scherb@users.noreply.github.com>
2021-12-02 21:04:54 +1300

    Abstract adjacent track to app.service and implement proxy rotation

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,6 @@
-import { findAdjacentTracks, Track, TrackList } from '1001-tracklists-scraper';
+import { TrackList, Track } from '1001-tracklists-scraper';
 import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { trackToAppearanceURLs } from './utils/helpers';
-import * as fs from "fs";
-import * as csv from "csv-parser";
-import * as path from "path";
 
 @Controller()
 export class AppController {
@@ -28,30 +24,6 @@ export class AppController {
   async findRelatedTracks (
     @Body("id") id: string
   ): Promise<any> {
-    const track = await this.appService.getTrack(id);
-    const tracklistUrls = trackToAppearanceURLs(track);
-
-    // Get the list of socks
-    const results = []
-    const getSocksProxies = new Promise((resolve) => {
-      fs.createReadStream(path.join(__dirname + "/.." +'/src/socks5.csv'))
-        .pipe(csv(["Proxy", "Region"]))
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            resolve(results);
-        });
-    });
-
-    const socksProxies = await getSocksProxies;
-    const scrapePromises: Promise<string[]>[] = [];
-
-    for (const i in tracklistUrls) {
-      const scrape = findAdjacentTracks(tracklistUrls[i], id, socksProxies[i].Proxy.replaceAll('"', ''));
-      scrapePromises.push(scrape);
-    }
-    
-    const adjacentTracks = await Promise.all(scrapePromises);
-
-    return Array.prototype.concat.apply([], adjacentTracks); // Nodes of all related tracks
+    return await this.appService.getAdjacentTracks(id);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,13 +1,51 @@
 import { Injectable } from '@nestjs/common';
-import { getTrack, getTracklist, Track, TrackInTracklist, TrackList, TracklistSearchResult } from '1001-tracklists-scraper';
+import { findAdjacentTracks, getTrack, getTracklist, Track, TrackInTracklist, TrackList, TracklistSearchResult } from '1001-tracklists-scraper';
+import * as fs from "fs";
+import * as csv from "csv-parser";
+import * as path from "path";
+import { trackToAppearanceURLs } from './utils/helpers';
 
 @Injectable()
 export class AppService {
+  socksProxies: string[] = [];
+  socksProxyListPosition: number = 0;
+  
+  constructor() {
+    const results = []
+    fs.createReadStream(path.join(__dirname + "/.." +'/src/socks5.csv'))
+      .pipe(csv(["Proxy", "Region"]))
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+          this.socksProxies = results.map(r => r.Proxy);
+          console.log(this.socksProxies)
+      });
+  }
+
   async getTracklist(url: string): Promise<TrackList | undefined> {
     return await getTracklist(url, null);
   }
 
   async getTrack(url: string): Promise<Track | undefined> {
     return await getTrack(url);
+  }
+
+  async getAdjacentTracks(id: string) {
+    const track = await this.getTrack(id);
+    const tracklistUrls = trackToAppearanceURLs(track);
+    const scrapePromises: Promise<string[]>[] = [];
+
+    for (const i in tracklistUrls) {
+      const scrape = findAdjacentTracks(tracklistUrls[parseInt(i)], id, this.socksProxies[parseInt(i)+this.socksProxyListPosition].replace(/"/g, ''));
+      scrapePromises.push(scrape);
+    }
+
+    // Proxy rotation
+    this.socksProxyListPosition += (this.socksProxyListPosition + tracklistUrls.length < this.socksProxies.length) 
+                                 ? tracklistUrls.length
+                                 : 0
+    
+    const adjacentTracks = await Promise.all(scrapePromises);
+
+    return Array.prototype.concat.apply([], adjacentTracks); // Nodes of all related tracks
   }
 }
